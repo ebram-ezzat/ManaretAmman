3a552279e8e01ad3c03126cc761006ebc2edb90d
Admin <Admin@DESKTOP-3FRRSPF>
2024-03-25 19:25:24 +0200

    working on settings

--- DataAccessLayer/Models/GetSettingsResult.cs
+++ DataAccessLayer/Models/GetSettingsResult.cs
@@ -7,6 +7,7 @@ namespace DataAccessLayer.Models
 {
     public partial class GetSettingsResult
     {
+       
         public int SettingID { get; set; }
         public decimal? AdditionalInWork { get; set; }
         public decimal? AdditionalInHoliday { get; set; }
@@ -15,10 +16,10 @@ namespace DataAccessLayer.Models
         public int? CalLeavesFromSalary { get; set; }
         public int? CalLateAsAdditional { get; set; }
         public int? DailyWorkInMinutes { get; set; }
-        public int? StartTimeINMinutes { get; set; }
+        public int? StartTimeInMinutes { get; set; }
         public int? EndTimeInMinutes { get; set; }
         public int? AllowedLate { get; set; }
-        public int? NoOfDaysBerMonth { get; set; }
+        public int NoOfDaysBerMonth { get; set; }
         public int? WithMorningExtra { get; set; }
         public int? CheckoutAfterMidNight { get; set; }
         public int? AllowedLateInLeave { get; set; }
@@ -35,15 +36,15 @@ namespace DataAccessLayer.Models
         public int? GlobalLocal { get; set; }
         public int? Test { get; set; }
         public int? WithBreak { get; set; }
-        public int? breaktime { get; set; }
+        public int? BreakTime { get; set; }
         public int ProjectID { get; set; }
         public string ProjectName { get; set; }
         public string CompanyName { get; set; }
         public string ReportPath { get; set; }
         public string ReportPathEn { get; set; }
-        public string imagepath { get; set; }
-        public string footertitle2 { get; set; }
-        public string footertitle1 { get; set; }
+        public string ImagePath { get; set; }
+        public string FooterTitle2 { get; set; }
+        public string FooterTitle1 { get; set; }
         public int? IsActive { get; set; }
         public int? LateDeductType { get; set; }
         public decimal? LateDeductPercentage { get; set; }
@@ -58,39 +59,73 @@ namespace DataAccessLayer.Models
         public string ShortDatePattern { get; set; }
         public string DateFormatForGrid { get; set; }
         public int? EarlyLeaveDeductType { get; set; }
-        public decimal? EarlyLeaveDeductPercentage { get; set; }
-        public int timezone { get; set; }
-        public DateTime? validateperiodto { get; set; }
-        public int ApprovalProcess { get; set; }
-        public int MultiUser { get; set; }
-        public int? ISDynamicShift { get; set; }
+        public int EarlyLeaveDeductPercentage { get; set; }
+        public int? Timezone { get; set; }
+        public DateTime? ValidatePeriodTo { get; set; }
+        public int? ApprovalProcess { get; set; }
+        public int? MultiUser { get; set; }
+        public int? IsDynamicShift { get; set; }
         public decimal? CompanySharePercent2 { get; set; }
         public decimal? SocialSecurityAmount2 { get; set; }
-        public int AllowedMonthlyLeaves { get; set; }
+        public int? AllowedMonthlyLeaves { get; set; }
         public int? MissingCheckoutValue { get; set; }
-        public int AllowedMonthlyVacation { get; set; }
-        public int LeaveWillDeductFromSalary { get; set; }
-        public decimal VacationDeductPercentage { get; set; }
-        public int IsAllowUserTologin { get; set; }
-        public int ExtraDaysWillBeAddedToSalry { get; set; }
-        public int AllowMultiCheckIn { get; set; }
-        public int BeforeContractPeriod { get; set; }
-        public string AttachementPath { get; set; }
-        public int ViewSocialSecurity { get; set; }
-        public int ZKFlag { get; set; }
+        public int? AllowedMonthlyVacation { get; set; }
+        public int? LeaveWillDeductFromSalary { get; set; }
+        public decimal? VacationDeductPercentage { get; set; }
+        public int? IsAllowUserToLogin { get; set; }
+        public int? ExtraDaysWillBeAddedToSalry { get; set; }
+        public int? AllowMultiCheckIn { get; set; }
+        public int? BeforeContractPeriod { get; set; }
+        public int? ViewSocialSecurity { get; set; }
+        public int? ZKFlag { get; set; }
         public string SalarySlipReportName { get; set; }
-        public int DetailWillBeInShifts { get; set; }
-        public decimal SocialSecurityAmount3 { get; set; }
+        public int? DetailWillBeInShifts { get; set; }
+        public decimal? SocialSecurityAmount3 { get; set; }
         public decimal? CompanySharePercent3 { get; set; }
         public decimal? LoanPercentage { get; set; }
-        public string MainBankID { get; set; }
+        public int? MainBankID { get; set; }
         public string HrManager { get; set; }
-        public decimal IsHourRateInShifts { get; set; }
-        public int ByWorkingHours { get; set; }
-        public int CompanyMayCLose { get; set; }
-        public int ViewEmployeeSalaryeport { get; set; }
+        public decimal? IsHourRateInShifts { get; set; }
+        public int? ByWorkingHours { get; set; }
+        public int? CompanyMayClose { get; set; }
+        public int? ViewEmployeeSalaryeport { get; set; }
         public string WindowsUserPassword { get; set; }
         public string WindowsUserName { get; set; }
         public string WindowsDomain { get; set; }
+        public int? CompanyHasUnderTestStatus { get; set; }
+        public int? SettingPage { get; set; }
+        public int? IsBasic { get; set; }
+        public string CheckinKey { get; set; }
+        public string CheckoutKey { get; set; }
+        public string BreakinKey { get; set; }
+        public string BreakoutKey { get; set; }
+        public decimal? CountLateWillDeductAsInDays { get; set; }
+        public decimal? CountEarlyLeaveWillDeductAsInDays { get; set; }
+        public int? IsWithApproval { get; set; }
+        public int? SSNINSalarySlip { get; set; }
+        public decimal? BreakLadeDeductPercentage { get; set; }
+        public int? BreakWillDeductFromLeaveInMinutes { get; set; }
+        public string SalaryReportAggregateName { get; set; }
+        public string LoanReportName { get; set; }
+        public string ReportBankConvertName { get; set; }
+        public int? ShowCompanyNameID { get; set; }
+        public int? EmailPort { get; set; }
+        public string EmailHost { get; set; }
+        public string EmailPassword { get; set; }
+        public string EmailFrom { get; set; }
+        public int? isviewAssuranceTemplte { get; set; }
+        public int? ShowEvaluationPage { get; set; }
+        public decimal? MissingBreakInValue { get; set; }
+        public decimal? MissingBreakOutValue { get; set; }
+        public string DailyAttendanceReportName { get; set; }
+        public string DailyAttendanceReportDetails { get; set; }
+        public string EmployeeReportDetailSalary { get; set; }
+        public string EmployeeReportDetailSalaryAll { get; set; }
+        public int? RamadanEndDate { get; set; }
+        public int? RamadanStartDate { get; set; }
+        public int? IsCompletedRamadanSetting { get; set; }
+        public int? EnableSecondPeriod { get; set; }
+        public string AttachementPath { get; set; }
+        public string IsviewTimeDr { get; set; }
     }
 }
