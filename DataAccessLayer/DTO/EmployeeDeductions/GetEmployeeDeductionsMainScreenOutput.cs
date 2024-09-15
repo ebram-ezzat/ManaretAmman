d8cc3037d8ff15529f01afd5c993ecc0dfe540fe
ebramezzat38 <ebramezzat38@gmail.com>
2024-09-15 20:24:19 +0200

    working on GetEmployeeDeductionsMainScreen

--- DataAccessLayer/DTO/EmployeeDeductions/GetEmployeeDeductionsMainScreenOutput.cs
+++ DataAccessLayer/DTO/EmployeeDeductions/GetEmployeeDeductionsMainScreenOutput.cs
@@ -10,16 +10,16 @@ namespace DataAccessLayer.DTO.EmployeeDeductions
     {
         public int? AllowanceID { get; set; }
         public string DefaultDesc { get; set; }
-        public int? EmployeeAllowanceID { get; set; }
+        public int? EmployeeDeductionID { get; set; }
         public int? EmployeeID { get; set; }
         public int? StartDate { get; set; }
         public int? EndDate { get; set; }
         public decimal? Amount { get; set; }
-        public decimal? AllowanceAmount { get; set; }         
-        public string WithOverTimeDesc { get; set; }
-        public int? CalculatedWithOverTime { get; set; }
+        public decimal? DeductionAmount { get; set; }         
+        //public string WithOverTimeDesc { get; set; }
+        //public int? CalculatedWithOverTime { get; set; }
         public string EmployeeName { get; set; }
-        public int? EmployeeNumber { get; set; }
+        //public int? EmployeeNumber { get; set; }
         public DateTime? v_StartDate { get; set; }
         public DateTime? v_EndDate { get; set; }
     }
